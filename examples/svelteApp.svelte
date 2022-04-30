9ef18ceef0c23528a7e1e31c5e3a8d0d103138da
Jimmy DC <jimmydalecleveland@gmail.com>
2022-04-29 20:07:34 -0600

    minor formatting

--- examples/svelteApp.svelte
+++ examples/svelteApp.svelte
@@ -4,7 +4,11 @@
 
 <main>
 	<h1 id="root">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the 
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a> 
+		to learn how to build Svelte apps.
+	</p>
 </main>
 
 <style>
